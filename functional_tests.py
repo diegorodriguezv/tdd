cf0f35fb962a3f4e5a513c4f7ba1b05a5e66139c
Diego Fernando Rodríguez Varón <diegorodriguezv@gmail.com>
2022-03-22 10:46:46 -0500

    Front page html generated from a template.

--- functional_tests.py
+++ functional_tests.py
@@ -30,7 +30,10 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(any(row.text == "1: play piano" for row in rows))
+        self.assertTrue(
+            any(row.text == "1: play piano" for row in rows),
+            "New to do item did not appear in table",
+        )
 
         # He enters "practice yoga"
         self.fail("Finish the test!")
