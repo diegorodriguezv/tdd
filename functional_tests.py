12403b28fe0aa58ede095e8ffb65fde600eaec31
Diego Fernando Rodríguez Varón <diegorodriguezv@gmail.com>
2022-03-22 09:42:46 -0500

    FT checks task input

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -7,21 +9,31 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-
         # Diego likes to do lists
         self.browser.get("http://localhost:8000")
 
         # He finds to-do in the title
         self.assertIn("To-Do", self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name("h1").text
+        self.assertIn("To-Do", header_text)
 
         # He wants to add a task
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
 
         # He adds "play piano"
+        inputbox.send_keys("play piano")
 
         # He hits enter and the task is added
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertTrue(any(row.text == "1: play piano" for row in rows))
 
         # He enters "practice yoga"
+        self.fail("Finish the test!")
 
         # He hits enter and the task is added
 
