ada50b72d506b184ecf73d2f6935dfc263611312
Diego Fernando Rodríguez Varón <diegorodriguezv@gmail.com>
2022-03-23 20:55:42 -0500

    Items are associated with a list

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -20,20 +20,28 @@ class NewListTest(TestCase):
         self.assertEqual(new_item.text, "A new list item")
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
         first_item = Item()
         first_item.text = "The first item"
+        first_item.list = list_
         first_item.save()
         second_item = Item()
         second_item.text = "item 2"
+        second_item.list = list_
         second_item.save()
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, "The first item")
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, "item 2")
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -42,8 +50,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, "list.html")
 
     def test_display_all_list_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
+        list_ = List.objects.create()
+        Item.objects.create(text="itemey 1", list=list_)
+        Item.objects.create(text="itemey 2", list=list_)
         response = self.client.get("/lists/theone/")
         self.assertContains(response, "itemey 1")
         self.assertContains(response, "itemey 2")
