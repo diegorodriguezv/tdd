0489dc17419aa2e8901dea724a35e9885efc4864
Diego Fernando Rodríguez Varón <diegorodriguezv@gmail.com>
2022-03-28 18:37:23 -0500

    First ft for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -9,9 +9,25 @@ POLL_INTERVAL = 0.5
 
 
 class NewVisitorTest(LiveServerTestCase):
-    # TODO: Refactor away some duplication in urls.py
     def setUp(self):
         self.browser = webdriver.Firefox()
+
+    def test_layout_and_styling(self):
+        # Diego goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] / 2, 512, delta=10
+        )
+        inputbox.send_keys("testing")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_table("1: testing")
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] / 2, 512, delta=10
+        )
 
     def test_can_start_a_list_for_one_user(self):
         # Diego likes to do lists
