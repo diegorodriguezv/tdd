db4953efbbc91145ed9ee466c2d7c9c4df387a34
Diego Fernando Rodríguez Varón <diegorodriguezv@gmail.com>
2022-04-26 15:00:30 -0500

    Fix logout button

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth.views import logout
 from accounts import views as accounts_views
 
 
@@ -7,4 +8,5 @@ urlpatterns = [
         r"^send_login_email$", accounts_views.send_login_email, name="send_login_email"
     ),
     url(r"^login$", accounts_views.login, name="login"),
+    url(r"^logout$", logout, {"next_page": "/"}, name="logout"),
 ]
