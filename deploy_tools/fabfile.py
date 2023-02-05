2e9c2a246c269626a7c4564c4ef7be5e27b5fac8
Diego Fernando Rodríguez Varón <diegorodriguezv@gmail.com>
2023-02-05 16:17:19 -0500

    Fix problems using secret key in fab run by removing the double quotes in .env

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -39,7 +39,7 @@ def _create_or_update_dotenv():
         key = "".join(
             random.SystemRandom().choices("abcdefghijklmnopqrstuvwxyz0123456789", k=50)
         )
-        append(".env", f'DJANGO_SECRET_KEY="{key}"')
+        append(".env", f"DJANGO_SECRET_KEY={key}")
 
 
 def _update_static_files():
